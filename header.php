0f1ff5f88405b9fa2cdfa9e0c983e2869f597b60
cf <nocinc@ymail.com>
2018-04-25 03:13:27 -0500

    Okay This Should Work

---
+++ header.php
@@ -0,0 +1,31 @@
+<?php
+$baseUrl = 'http://localhost/nocinc18/';
+$css = 'http://localhost/nocinc18/css/';
+$js = 'http://localhost/nocinc18/js';
+$plugins = 'http://localhost/nocinc18/plugins/';
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>CCF Computing</title>
+
+        <!-- Bootstrap -->
+        <link href="<?php echo $css; ?>bootstrap.min.css" rel="stylesheet">
+        <link href="<?php echo $plugins; ?>font-awesome/css/font-awesome.min.css" rel="stylesheet">
+        <link href="<?php echo $css; ?>main.css" rel="stylesheet">
+        <link href="<?php echo $css; ?>reset.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+    </head>
+    <body>
