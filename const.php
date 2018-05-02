3fbc234649fa5f928660705f2d82d87090d6170f
cf <nocinc@ymail.com>
2018-05-02 00:22:27 -0500

    const.php

--- const.php
+++ const.php
@@ -1,4 +1,4 @@
 <?php
 //define('BASE_URL', 'http://ccfcomputing.com/');
-define('BASE_URL', 'http://localhost/nocinc18/');
-define('BASE_URI', 'http://localhost/nocinc18/load.php');
+define('BASE_URL', 'http://ccfcomputing.com/');
+define('BASE_URI', 'http://ccfcomputing.com/load.php');
