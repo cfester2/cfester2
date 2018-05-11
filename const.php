d500849dfd1719c029d239002966ae38c08d42e8
cf <nocinc@ymail.com>
2018-05-11 02:19:02 -0500

    grrr

--- const.php
+++ const.php
@@ -6,8 +6,8 @@
 //define('DBPASS', '!*LFE;*6[@c?');
 //define('DBDB', 'nocinc');
 //LOCAL
-define('BASE_URL', 'http://localhost/nocinc18/');
-define('BASE_URI', 'http://localhost/nocinc18/load.php');
+define('BASE_URL', 'http://ccfcomputing.com/');
+define('BASE_URI', 'http://ccfcomputing.com/load.php');
 define('DBUSER', 'root');
 define('DBPASS', '');
 define('DBDB', 'nocinc');
